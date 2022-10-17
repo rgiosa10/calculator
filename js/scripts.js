8dd6e76fc735f7db299d54fc2bc7f023c6878ed1
Ruben Giosa <rubenalexgiosa@gmail.com>
2022-10-17 13:59:16 -0700

    Add functions for subtraction, multiplication, and division with consolidated alert message
    Co-authored-by: Jennifer Holcomb <jenniferlholcomb@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,29 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
 //user interface logic
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-const additionalResult = add(number1,number2)
-window.alert(additionalResult)
+const addResults = add(number1,number2)
+const subResults = subtract(number1,number2)
+const multResults = multiply(number1,number2)
+const divResults = divide(number1,number2)
+
+const finalResults = "The addition of your numbers equals " + addResults + ".\nThe subtraction of your numbers equals " + subResults + ".\nThe multiplication of your numbers equals " + multResults + ".\nThe division of your numbers equals " + divResults + "."
+
+window.alert(finalResults)
+
+
