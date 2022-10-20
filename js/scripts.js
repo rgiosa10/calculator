4e1efc2880f683e921f2dc1ca29c426debff43e9
Ruben Giosa <rubenalexgiosa@gmail.com>
2022-10-20 13:37:48 -0700

    Update calculate function to remove providing results, and moved it under user interface logic
    Co-authored-by: Kirsten Opstad <kirsten.opstad@gmail.com>
    Co-authored-by: Alejandro Socarras <apsocarras@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,7 @@ function calculate() {
   const number2Input = parseInt(document.getElementById("input2").value);
   const operatorInput = document.querySelector("input[name='operator']:checked").value;
 
+  let result;
   if (operatorInput === "add") {
     result = add(number1Input, number2Input);
   } else if (operatorInput === "subtract") {
@@ -29,7 +30,7 @@ function calculate() {
   } else if (operatorInput === "divide") {
     result = divide(number1Input, number2Input);
   }
-  document.getElementById("output").innerText = "The answer is " + result;
+  return result;
 }
 
 //user interface logic
@@ -39,41 +40,6 @@ window.addEventListener("load", function() {
 
   form.addEventListener("submit", function(event) {
     event.preventDefault();
+    document.getElementById("output").innerText = "The answer is " + calculate();
   });
-
-  // form.addEventListener("submit", getFormInputs);
-
-  form.addEventListener("submit", calculate);
-
-    // event.preventDefault()
-    // form.addEventListener("submit", getFormInputs);
-    // form.addEventListener("submit", calculate);
-    
-  });
-
-
-// Separating calculation and obtaining the form inputs, using arrays
-
-  // function calculate(array) {
-  //   if (array[2] === "add") {
-  //     result = add(array[0], array[1]);
-  //   } else if (array[2] === "subtract") {
-  //     result = subtract(array[0], array[1]);
-  //   } else if (array[2] === "multiply") {
-  //     result = multiply(array[0], array[1]);
-  //   } else if (array[2] === "divide") {
-  //     result = divide(array[0], array[1]);
-  //   }
-  //   document.getElementById("output").innerText = "The answer is " + result;
-  // }
-  
-  // function getFormInputs() {
-  //   const number1Input = parseInt(document.getElementByID("input1").value);
-  //   const number2Input = parseInt(document.getElementByID("input2").value);
-  //   const operatorInput = document.querySelector("input[name='operator']:checked").value;
-  
-  //   return [number1Input,number2Input,operatorInput];
-  // };
-
-  // Then can run calculate(getFormInputs())
-  
+});
